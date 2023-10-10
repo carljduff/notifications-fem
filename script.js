bfbc0d261db69d661981af5d99be0e261af79019
carljduff <carljduff@gmail.com>
2023-10-10 14:58:57 -0400

    updated styling

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let container = document.createElement("div");
 container.className = "container"
 app.append(container);
 let count = 0; 
-
-
+let frame;
+let circle;
 let headerWrapper = document.createElement("div");
 headerWrapper.className = "header-wrapper";
 let title = document.createElement("h1");
@@ -19,17 +19,18 @@ numOfNotifications.className = "nums"
 
 let buttonContainer = document.createElement("div");
 let markAsRead = document.createElement("button");
+markAsRead.className = "read-button"
 markAsRead.addEventListener("click", markAll)
 markAsRead.innerHTML = "Mark all as read";
-buttonContainer.className = "read-button";
+buttonContainer.className = "btn-container";
 buttonContainer.append(markAsRead);
 headerWrapper.append(title, numOfNotifications, buttonContainer);
 container.append(headerWrapper)
 
 
 data.users.map((item) => {
-    let frame = document.createElement("div");
-    frame.className = "frame"
+    frame = document.createElement("div");
+    frame.className = "frame bgcolor"
     let img = document.createElement("img");
     img.src = item.img;
     let name = document.createElement("p");
@@ -42,17 +43,30 @@ data.users.map((item) => {
         link.innerText = item.link;
         frame.append(link)
     }
-
-    if(item.message !== false) {
-        let message = document.createElement("p")
-        message.innerText = item.message;
-        frame.append(message)
+    
+    if(item.picMessage) {
+        let messagePic = document.createElement("img");
+        messagePic.src = item.picMessage;
+        messagePic.className = "messagePic";
+        frame.append(messagePic)
     }
 
     if(item.read === false) {
         count++;
         numOfNotifications.innerHTML = count;
+        circle = document.createElement("div");
+        circle.className = "circle";
+        frame.append(circle);
     }
+
+    if(item.message !== false) {
+        let message = document.createElement("p")
+        message.className = "message";
+        message.innerText = item.message;
+        frame.append(message)
+    }
+    
+    
 
     container.append(frame);
 })
@@ -62,6 +76,17 @@ function markAll() {
     data.users.forEach((item) => {
         item.read = true;
         numOfNotifications.innerHTML = 0;
+        let circles = document.querySelectorAll(".circle");
+
+        circles.forEach((item) => {
+            item.classList.remove("circle");
+        })
+
+        let frame = document.querySelectorAll(".bgcolor");
+
+        frame.forEach((item) => {
+            item.classList.remove("bgcolor")
+        })
     })
 }
 
